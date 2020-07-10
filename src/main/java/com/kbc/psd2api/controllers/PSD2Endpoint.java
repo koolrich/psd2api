91237641acd01a0c50e87f7b2bc08fef79bfe652
Richard <rnduka@temenos.com>
2020-07-10 12:02:33 +0100

    Added support for AIS permissions

--- src/main/java/com/kbc/psd2api/controllers/PSD2Endpoint.java
+++ src/main/java/com/kbc/psd2api/controllers/PSD2Endpoint.java
@@ -58,7 +58,8 @@ public class PSD2Endpoint {
         System.out.println("Get consent called with consentId: " + consentId);
 
         ConsentDetails consentDetails = new ConsentDetails();
-        consentDetails.setStatus("AUTHORISED");
+        //consentDetails.setStatus("AUTHORISED");
+        consentDetails.setStatus("AWAITING.AUTHORISATION");
         InstructedAmount instructedAmount = new InstructedAmount("20.00", "EUR");
         List<LinkedAccount> linkedAccounts = Arrays.asList(new LinkedAccount("CurrentAccount", "IE60ICON99027013046"), new LinkedAccount("SavingsAccount", "IE55ICON99027011885911"));
         CreditorAccount creditorAccount = new CreditorAccount("IE55ICON99027011885911", "Bob Clements");
